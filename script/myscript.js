df97249c266f5f809d65e3466720cbdf6d9bb110
Maithreyee M S <67473870+Maithreyee-Iyengar@users.noreply.github.com>
2021-05-03 18:06:30 +0530

    Add files via upload

---
+++ script/myscript.js
@@ -0,0 +1,8 @@
+function toggleApps(){
+      let appDiv = document.getElementById("applist");
+  if (appDiv.style.display === "none") {
+    appDiv.style.display = "block";
+  } else {
+    appDiv.style.display = "none";
+  }
+}
